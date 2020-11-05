9a282c58ca9771d25cfc6584e929f7f61f78615c
lurkor <lurkortv@gmail.com>
2020-11-04 23:53:04 -0800

    Messing around with arrays

--- DSA2/Program.cs
+++ DSA2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
 
 namespace DSA2
 {
@@ -7,19 +9,26 @@ namespace DSA2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] array1 = { 5, 3, 6, 8, 9, 10, 3, 7 };
+            int[] array1 = { 5, 3, 6, 8, 9, 3, 6, 11, 9, 10, 3, 7 };
             int largest = FindLargest(array1);
             int smallest = FindSmallest(array1);
+            int[] duplicates = FindDuplicates(array1);
+            string duplicateString = "";
             Console.WriteLine("The largest integer in { 5, 3, 6, 8, 9, 10, 3, 7 } is " + largest);
             Console.WriteLine("The smallest integer in { 5, 3, 6, 8, 9, 10, 3, 7 } is " + smallest);
+            for(int i = 0; i < duplicates.Length; i++)
+            {
+                duplicateString += " " + duplicates[i] + ",";  
+            }
+            Console.WriteLine("The duplicates in { 5, 3, 6, 8, 9, 10, 3, 7 } are" + duplicateString);
 
         }
         static int FindLargest(int[] arr)
         {
             int largest = arr[0];
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                if(arr[i] >= largest)
+                if (arr[i] >= largest)
                 {
                     largest = arr[i];
                 }
@@ -30,14 +39,39 @@ namespace DSA2
         static int FindSmallest(int[] arr)
         {
             int smallest = arr[0];
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                if(arr[i] <= smallest)
+                if (arr[i] <= smallest)
                 {
                     smallest = arr[i];
                 }
             }
             return smallest;
+        }
+
+        static int[] FindDuplicates(int[] arr)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (!list.Contains(arr[i]))
+                    {
+                        if (arr[i] == arr[j])
+                        {
+                            list.Add(arr[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+            int[] answer = new int[list.Count];
+            for(int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = list[i];
+            }
+            return answer;
         }
     }
 }
