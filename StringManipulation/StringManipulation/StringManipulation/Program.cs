88917d29672965e17706506ec6293a21dba0571f
lurkor <lurkortv@gmail.com>
2021-01-11 08:17:15 -0800

    Reversed a string method

---
+++ StringManipulation/StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello World!";
+
+            Console.WriteLine(ReverseString(helloWorld));
+        }
+        static string ReverseString(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            string answer = "";
+            for(int i = inputArray.Length; i > 0; i--)
+            {
+                answer += inputArray[i -1];
+            }
+            return answer;
+        }
+    }
+}
